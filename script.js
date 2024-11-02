535ce4b7bc483f0863154839d4c501d7e1b3e50b
CaptainZingers <99506297+CaptainZingers@users.noreply.github.com>
2024-11-03 00:08:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add JavaScript for animations, hover effects, etc.
+function hoverEffect(element) {
+    element.style.backgroundColor = "#333";
+    element.style.color = "#fff";
+}
+
+function removeEffect(element) {
+    element.style.backgroundColor = "transparent";
+    element.style.color = "#333";
+}
